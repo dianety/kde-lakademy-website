a4e8231e19c907701798beb7901e7538236b7b24
Lucas Angeli <lucas.angeli.gimenes@gmail.com>
2018-05-14 22:57:31 +0100

    Criação na página de 2017

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-/*Adiciona a classe para ativar a função de MediaQuery*/
+/*Adicione e remove a classe hide aos elementos que possuem a classe .navbar-item-collapse*/
 function menuShow() {
     var items = document.getElementsByClassName("navbar-item-collapse");
 
@@ -9,10 +9,28 @@ function menuShow() {
             items[i].classList.add("hide");
         }
     }
+
+    if( dropdownIsOpen() ){
+        dropdownShow();
+    }
 }
 
-function scroll()  {
-    var element = document.getElementById("sobre");
-    console.log( element ) ;
-    window.scrollTo(element.offsetLeft, element.offsetTop);
+/*Adicione e remove a classe hide aos elementos que possuem a classe .navbar-collapse-dropdown-item*/
+function dropdownShow() {
+    var items = document.getElementsByClassName("navbar-collapse-dropdown-item");
+
+    for (i = 0; i < items.length; i++) {
+        if (items[i].classList.contains("hide")) {
+            items[i].classList.remove("hide")
+        } else {
+            items[i].classList.add("hide");
+        }
+    }
 }
+
+function dropdownIsOpen(){
+    var items = document.getElementsByClassName("navbar-collapse-dropdown-item");
+    return !items[i].classList.contains("hide") ;
+}
+
+
