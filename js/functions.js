54567707cb6b8d70472ee9b9071ec6d27ce73451
Lucas Angeli <lucas.angeli.gimenes@gmail.com>
2018-05-23 21:52:28 +0100

    Pagina Inscreva-se

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-/*Adicione e remove a classe hide aos elementos que possuem a classe .navbar-item-collapse*/
+
 function menuShow() {
     var items = document.getElementsByClassName("navbar-item-collapse");
 
@@ -15,7 +15,7 @@ function menuShow() {
     }
 }
 
-/*Adicione e remove a classe hide aos elementos que possuem a classe .navbar-collapse-dropdown-item*/
+
 function dropdownShow() {
     var items = document.getElementsByClassName("navbar-collapse-dropdown-item");
 
@@ -33,4 +33,42 @@ function dropdownIsOpen(){
     return !items[i].classList.contains("hide") ;
 }
 
+function validateForm(){
 
+    var msg = "", nome = "", email = "", type = "", utilizador = "", dominio="" ;
+
+    var nome  = document.getElementById( "nome" ).value;
+    var email = document.getElementById( "email" ).value;
+
+    if( nome != "" && email != "" ){
+
+        if ( email.indexOf('@') != -1 && email.indexOf('.') != -1 ) {
+
+            utilizador = email.substring( 0, email.indexOf('@') ) ;
+            dominio = email.substring( email.indexOf('@') , email.length ) ;
+
+            if ( utilizador.length >= 1 && dominio.length >= 3 ){
+                msg = "Sucesso! Sua inscrição foi confirmada!"
+                type = "success-message";
+            } else {
+                msg = "Informe um e-mail correto!"
+                type = "error-message";
+            }
+        }else{
+            msg = "Informe um e-mail correto!"
+            type = "error-message";
+        }
+    } else {
+        msg="Informe os campos corretamente!"
+        type = "error-message";
+    }
+
+    document.getElementById( 'form-output').innerHTML = "<p class=\""+type+"\"> " + msg + "</>"
+
+}
+
+function cleanOutput(){
+    if( document.getElementById('form-output').innerHTML.match( "error-message" ) ){
+        document.getElementById('form-output').innerHTML = "" ;
+    }
+}
