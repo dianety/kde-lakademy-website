de4b995e3779d3ad4601635cee925997a3157bf6
Alessandro Feitoza <eu@alessandrofeitoza.eu>
2018-07-07 17:20:37 -0300

    Removing elses and improving code reading

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,15 @@
 
-function menuShow() {
-    var items = document.getElementsByClassName("navbar-item-collapse");
+function menuShow()
+{
+    let items = document.getElementsByClassName("navbar-item-collapse");
 
     for( i=0 ; i < items.length ; i++ ){
         if (items[i].classList.contains("hide")) {
-            items[i].classList.remove("hide")
-        } else {
-            items[i].classList.add("hide");
+            items[i].classList.remove("hide");
+            continue;
         }
+
+        items[i].classList.add("hide");
     }
 
     if( dropdownIsOpen() ){
@@ -16,55 +18,77 @@ function menuShow() {
 }
 
 
-function dropdownShow() {
-    var items = document.getElementsByClassName("navbar-collapse-dropdown-item");
+function dropdownShow()
+{
+    let items = document.getElementsByClassName("navbar-collapse-dropdown-item");
 
     for (i = 0; i < items.length; i++) {
         if (items[i].classList.contains("hide")) {
-            items[i].classList.remove("hide")
-        } else {
-            items[i].classList.add("hide");
+            items[i].classList.remove("hide");
+            continue;
         }
+
+        items[i].classList.add("hide");
     }
 }
 
-function dropdownIsOpen(){
-    var items = document.getElementsByClassName("navbar-collapse-dropdown-item");
+function dropdownIsOpen()
+{
+    let items = document.getElementsByClassName("navbar-collapse-dropdown-item");
     return !items[i].classList.contains("hide") ;
 }
 
-function validateForm(){
+function validateForm()
+{
+    let msg = "", type = "", utilizador = "", dominio="" ;
 
-    var msg = "", nome = "", email = "", type = "", utilizador = "", dominio="" ;
+    let nome  = document.getElementById( "nome" ).value;
+    let email = document.getElementById( "email" ).value;
 
-    var nome  = document.getElementById( "nome" ).value;
-    var email = document.getElementById( "email" ).value;
-
-    if( nome != "" && email != "" ){
-
-        if ( email.indexOf('@') != -1 && email.indexOf('.') != -1 ) {
-
-            utilizador = email.substring( 0, email.indexOf('@') ) ;
-            dominio = email.substring( email.indexOf('@') , email.length ) ;
-
-            if ( utilizador.length >= 1 && dominio.length >= 3 ){
-                msg = "Sucesso! Sua inscrição foi confirmada!"
-                type = "success-message";
-            } else {
-                msg = "Informe um e-mail correto!"
-                type = "error-message";
-            }
-        }else{
-            msg = "Informe um e-mail correto!"
-            type = "error-message";
-        }
-    } else {
-        msg="Informe os campos corretamente!"
+    if( nome == "" || email == "" ) {
+        msg = "Nome e Email são obrigatórios!";
         type = "error-message";
+
+        generateFormValidationMessage(msg, type);
+
+        return;
     }
 
-    document.getElementById( 'form-output').innerHTML = "<p class=\""+type+"\"> " + msg + "</>"
+    if (validateEmailForRegister(email) !== true) {
+        return;
+    }
 
+    msg = "Sucesso! Sua inscrição foi confirmada!";
+    type = "success-message";
+
+    generateFormValidationMessage(msg, type);
+}
+
+function generateFormValidationMessage (msg, type)
+{
+    document.getElementById( 'form-output').innerHTML = "<p class=\""+type+"\"> " + msg + "</>"
+}
+
+function validateEmailForRegister(email)
+{
+    if ( email.indexOf('@') == -1 || email.indexOf('.') == -1 ) {
+        msg = "Informe um e-mail correto!"
+        type = "error-message";
+
+        return generateFormValidationMessage(msg, type);
+    }
+
+    utilizador = email.substring( 0, email.indexOf('@') ) ;
+    dominio = email.substring(utilizador.length + 1);
+
+    if ( utilizador.length < 1 || dominio.length < 4 ) {
+        msg = "Informe um e-mail correto!";
+        type = "error-message";
+
+        return generateFormValidationMessage(msg, type);
+    }
+
+    return true;
 }
 
 function cleanOutput(){
